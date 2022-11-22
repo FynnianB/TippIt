79118f42848a19f0ecc2059a150c57031d0a3dcf
FynnianB <61945444+FynnianB@users.noreply.github.com>
2022-11-22 11:40:07 +0000

    Deploying to gh-pages from  @ af56afbe69484f8546ed54eb0064f7bea5b53a3b 🚀

---
+++ precache-manifest.2b5d71bc41d7a49df429fa88c0d0b58e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a42d949d812ef534804",
+    "url": "/TippIt/css/app.37832257.css"
+  },
+  {
+    "revision": "e67704026c4b1ac1cbe0",
+    "url": "/TippIt/css/chunk-002ebaa8.f63f36fb.css"
+  },
+  {
+    "revision": "f36c0c53280430fff68c",
+    "url": "/TippIt/css/chunk-17660fca.b417d44c.css"
+  },
+  {
+    "revision": "7db59f6630865cc5a706",
+    "url": "/TippIt/css/chunk-5b23bc5f.5f628237.css"
+  },
+  {
+    "revision": "8db5954b1b89d3454a1f",
+    "url": "/TippIt/css/chunk-826cc6e2.f42260bb.css"
+  },
+  {
+    "revision": "2a0263d8f5fb81999f8f",
+    "url": "/TippIt/css/chunk-8bd0ad98.c3b07fa1.css"
+  },
+  {
+    "revision": "107cccb56770f4f54e22",
+    "url": "/TippIt/css/chunk-90af8d66.9600e10d.css"
+  },
+  {
+    "revision": "8d13ba6c7b279b9611f3",
+    "url": "/TippIt/css/chunk-a44caee2.051cf743.css"
+  },
+  {
+    "revision": "3021afcb89779240367a",
+    "url": "/TippIt/css/chunk-c76440ec.1a31b591.css"
+  },
+  {
+    "revision": "69272996ab261179e2cc",
+    "url": "/TippIt/css/chunk-vendors.08bbbb09.css"
+  },
+  {
+    "revision": "b4c78bb685619826d863fd81f748e91e",
+    "url": "/TippIt/img/logo.b4c78bb6.svg"
+  },
+  {
+    "revision": "9efaf8516a440483f76333d6555facfc",
+    "url": "/TippIt/index.html"
+  },
+  {
+    "revision": "0a42d949d812ef534804",
+    "url": "/TippIt/js/app.69ad77ed.js"
+  },
+  {
+    "revision": "e67704026c4b1ac1cbe0",
+    "url": "/TippIt/js/chunk-002ebaa8.398c87b8.js"
+  },
+  {
+    "revision": "f36c0c53280430fff68c",
+    "url": "/TippIt/js/chunk-17660fca.2877d36a.js"
+  },
+  {
+    "revision": "9412aca1e19a593b5275",
+    "url": "/TippIt/js/chunk-30d1fbfd.54192a01.js"
+  },
+  {
+    "revision": "7db59f6630865cc5a706",
+    "url": "/TippIt/js/chunk-5b23bc5f.34cdb262.js"
+  },
+  {
+    "revision": "8db5954b1b89d3454a1f",
+    "url": "/TippIt/js/chunk-826cc6e2.43e85b1c.js"
+  },
+  {
+    "revision": "2a0263d8f5fb81999f8f",
+    "url": "/TippIt/js/chunk-8bd0ad98.9cdeddc9.js"
+  },
+  {
+    "revision": "107cccb56770f4f54e22",
+    "url": "/TippIt/js/chunk-90af8d66.90159bf0.js"
+  },
+  {
+    "revision": "8d13ba6c7b279b9611f3",
+    "url": "/TippIt/js/chunk-a44caee2.fc185d27.js"
+  },
+  {
+    "revision": "3021afcb89779240367a",
+    "url": "/TippIt/js/chunk-c76440ec.341e9bd2.js"
+  },
+  {
+    "revision": "69272996ab261179e2cc",
+    "url": "/TippIt/js/chunk-vendors.3734f166.js"
+  },
+  {
+    "revision": "4fd2dd41d846619dc35f4e8743189ee0",
+    "url": "/TippIt/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TippIt/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d661a4e8b07436d0860c3ac5aff63c0a.js"
+  "/TippIt/precache-manifest.2b5d71bc41d7a49df429fa88c0d0b58e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tippit"});
